59cbedcabab25656e3f1c776e6f2f4b2c6900392
dineshreddy8742 <palavaladineshkumarreddy17@gmail.com>
2025-11-07 16:31:29 +0530

    fix: Update vite config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/profile/", // <-- Replace 'profile' with your actual GitHub repo name
+    base: "/", // <-- Replace 'profile' with your actual GitHub repo name
     plugins: [react()],
     resolve: {
         alias: {
